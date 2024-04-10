30c5918789c77f9d56082763380689a1a17a7b83
gangx <gangx6906@gmail.com>
2024-04-11 01:42:28 +0800

    测试

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
 func main() {
+	server := NewHttpServer("server1") //创建服务
 
+	server.Route("GET", "/", func(ctx *Context) {
+		ctx.WriteJsonOK("hello world")
+	}) //注册服务的路由信息
+	server.Start(":8080") //启动服务
 }
