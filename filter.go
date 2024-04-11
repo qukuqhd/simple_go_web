624e3dfb0fdeea28d3c03c24ce6a322025aae478
gangx <gangx6906@gmail.com>
2024-04-11 16:39:46 +0800

    aop处理

--- filter.go
+++ filter.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type HandlerFunc func(c *Context)
+type Filter func(c *Context)
+type FilterBuilder func(next Filter) Filter
+
+var _ FilterBuilder = MetricsFilterBuilder
+
+func MetricsFilterBuilder(next Filter) Filter {
+	return func(c *Context) {
+		start := time.Now().Nanosecond()
+		next(c)
+		end := time.Now().Nanosecond()
+		fmt.Printf("cost:%d\n", end-start)
+	}
+}
