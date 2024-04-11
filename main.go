0e39dde8066f56ed004d84a2cbee67f47c8715a0
gangx <gangx6906@gmail.com>
2024-04-11 16:40:53 +0800

    测试

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
+import "net/http"
+
 func main() {
 	server := NewHttpServer("server1") //创建服务
 
-	server.Route("GET", "/", func(ctx *Context) {
+	server.Route(http.MethodGet, "/", func(ctx *Context) {
 		ctx.WriteJsonOK("hello world")
 	}) //注册服务的路由信息
 	server.Start(":8080") //启动服务
